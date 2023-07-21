cd622c19430e4a61e8e8db7d45596816b1b96f66
Diana Chacón <diianachm1802@gmail.com>
2023-07-21 05:36:53 +0000

    funcionalidades listas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,93 @@
-import React from "react";
-import Input from "./input.jsx"
-
+import React, { useEffect, useState} from "react";
+import '../../styles/index.css';
+import Task from "./task.jsx";
 
 const Home = () => {
+	const [tasks, setTasks] = useState([])
+	const [inputText, setInputText] = useState("");
+	const Api = 'https://fake-todo-list-52f9a4ed80ce.herokuapp.com/todos/user/Viridian-a'
+
+	useEffect(() => {
+		fetch(Api).then(result => result.json()).then(data => {
+			console.log(data)
+			setTasks(data)
+		}
+		).catch(err => err)
+	}, []);
+
+
+
+	const updateAllTasks = () => {
+		const newTask = { done: false, id: Date.now(), label: inputText };
+
+		fetch(Api, {
+			method: 'PUT',
+			body: JSON.stringify([...tasks, newTask]),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).catch(err => err);
+
+		setTasks([...tasks, newTask]);
+		setInputText('');
+	}
+
+	const handleKeyDown = (e) => {
+		if (e.key === "Enter") {
+			if (inputText.trim() !== '') {
+				updateAllTasks()
+			}
+		}
+	}
+
+
+	const deleteTask = (index) => {
+		fetch(Api, {
+			method: 'PUT',
+			body: JSON.stringify(tasks.filter((_, i) => i !== index)),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).catch(err => err);
+		setTasks(tasks.filter((_, i) => i !== index));
+	}
+
+	const deleteAllTask = () => {
+		fetch(Api, {
+			method: 'PUT',
+			body: JSON.stringify([]),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).catch(err => err);
+		setTasks([]);
+	}
+
 	return (
 		<div className="general__container">
-			<h1 className="text-center mt-5">To-do List</h1>
-			<div className="d-flex justify-content-center">
-				<div className="inputs__container">
-					<Input/>
-				</div>
-			</div>
-			
+			<h1 className="text-center mt-5">Todos</h1>
+			<ul className="list__container">
+				<li className="task">
+					<input type="text" 
+					id="inputField"
+					 value={inputText} placeholder="What needs to be done?" onKeyDown={handleKeyDown} onChange={(e) => { setInputText(e.target.value) }} />
+				</li>
+				{/* Render every input that gets inserted */}
+				{tasks.map((task, index) => {
+					return (
+						<li key={index} className="task">
+							<Task task={task.label} deleteTask={deleteTask} id={index} />
+						</li>
+					);
+				})}
+				<li className="items__counter">{tasks.length} Items left</li>
+				<button onClick={() => { deleteAllTask() }}>DELETE ALL</button>
+			</ul>
+
 		</div>
 	);
+
+
 };
 
 export default Home;
